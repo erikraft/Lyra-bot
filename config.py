fc56dc9857c8e096b7a18674e99cc14c590858e2
erikraft <erikraft43@gmail.com>
2025-07-30 19:20:33 -0300

    Update ;--

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 id_do_servidor = 123456789012345678  # Substitua pelo ID do seu servidor
 CANAL_PAINEL_ID = 123456789012345678  # Substitua pelo ID do canal do painel de ticket
 id_cargo_atendente = 123456789012345678  # Substitua pelo ID do cargo de atendente
-TOKEN = "A1B2C3D4E5F6G7H8I9J0K1L2M3N4O5P6Q7R8S9T0"  # Substitua pelo token do seu bot
+TOKEN = "##"  # Substitua pelo token do seu bot
 ID_CANAL_LOGS = 123456789012345678  # Substitua pelo ID do canal de logs
 ID_CANAL_MOD =  123456789012345678  # Substitua pelo ID do canal de punição publico
-link_apelacao = "https://forms.gle/1a2b3c4d5e6f7g8h9i" # Substitua pelo link do formulário de apelação
+link_apelacao = "https://form.jotform.com/242088340178054" # Substitua pelo link do formulário de apelação
